995145f79efff187801f35b44ce0990eec28fc4f
mankyb0mb <rainerb0716@gmail.com>
2024-05-03 12:07:52 -0700

    one and two done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(text, times) {
+    if (times < 0) {
+        return "ERROR"
+    }
+    else {
+        var repeated = text.repeat(times);
+        console.log(repeated)
+        return repeated
+    }
 
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
