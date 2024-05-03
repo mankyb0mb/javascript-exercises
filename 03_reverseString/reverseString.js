858cb892f6dc2eeefb31ff5cffd7452417a24eaa
mankyb0mb <rainerb0716@gmail.com>
2024-05-03 12:37:36 -0700

    assignment 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(rawString) {
+    let newString = ""
+    // Iterates through each character of the string backwards
+    for (let i = 1; i <= rawString.length; i++) {   
+        // Each iteration adds itself to the string (eg: "backwards" -> "s" + "d" = "sd" + "r" = "sdr"...."sdrawkcab") 
+        newString = newString + rawString.at(-(i))
+    }
+    return newString
 };
 
 // Do not edit below this line
