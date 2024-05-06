c18740809b07b80d885c11cf7ef828c0542ae3ad
mankyb0mb <rainerb0716@gmail.com>
2024-05-06 16:22:37 -0700

    09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    var cleanString = (string.replace(/[^\w]/g, "")).toLowerCase();
+    let reverseString = "";
+    for (let i = 1; i <= cleanString.length; i++) {
+        reverseString = reverseString + cleanString.at(-i)
+    }
+    if (cleanString == reverseString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
