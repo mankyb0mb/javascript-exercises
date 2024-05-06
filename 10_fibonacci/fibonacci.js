1bd717dd4d9494fdc8b05fe80c1056c8dcb00745
mankyb0mb <rainerb0716@gmail.com>
2024-05-06 16:44:50 -0700

    10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let a = 1
+    let b = 1
+    if (index < 0) return "OOPS";
+    else if (index == "" || index == 0) return 0;
+    else {
+        for (let i = 2; i < index; i++) {
+            let prevb = b
+            b = a + b
+            a = prevb
+        }
+    }
+    return b;
 };
 
+console.log(fibonacci(4))
 // Do not edit below this line
 module.exports = fibonacci;
