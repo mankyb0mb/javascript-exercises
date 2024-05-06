61ad279f71ace918cba4ad8afd86a0c0a6009343
mankyb0mb <rainerb0716@gmail.com>
2024-05-06 14:29:19 -0700

    05 ez range adder

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    var sum = "ERROR"
+    if ((typeof(x) != "number") || (typeof(y) != "number"))  {
+        return sum;
+    }
+    else if ((x < 0) || (y < 0)) {
+        return sum;
+    }
+    else {
+        if (x < y) {
+            var start = x;
+            var end = y;
+        }
+        else {
+            var start = y;
+            var end = x;
+        }
+        // example start = 4, end = 7; 4 + 5 + 6 + 7
+        sum = 0;
+        for (let adder = start; adder <= end; adder++) {
+            sum = sum + adder;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
