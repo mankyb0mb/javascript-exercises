05385363968e49413dacd6fe056b5b72fd236b5e
mankyb0mb <rainerb0716@gmail.com>
2024-05-06 15:03:41 -0700

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  num = (num - 32) * 5/9
+  num = parseFloat(num.toFixed(1))
+  return num;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  num = (num * 9/5) + 32
+  num = parseFloat(num.toFixed(1))
+  return num;
 };
 
 // Do not edit below this line
