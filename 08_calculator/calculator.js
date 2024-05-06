3f05c32ca3f4e75371e4e41e7e35f58ae1060d12
mankyb0mb <rainerb0716@gmail.com>
2024-05-06 15:22:06 -0700

    08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	var output = 0;
+  for (let i = 0; i < array.length; i++) {
+    output = output + array[i]
+  }
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function(multi) {
+  var sum = 1;
+  for (let i = 0; i < multi.length; i++) {
+    sum = sum * multi[i]
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+  var output = 1;
+  for (let i = 1; i <= fact; i++) {
+    output = output * i
+  }
+  return output;
 };
 
 // Do not edit below this line
