0cd9d773e9c4c692a8748e234f149ccd4c6b445b
mankyb0mb <rainerb0716@gmail.com>
2024-05-11 20:23:12 -0700

    caesar

--- archive/archived_caesar/caesar.js
+++ archive/archived_caesar/caesar.js
@@ -1,5 +1,27 @@
-const caesar = function() {
+const alphbet = "abcdefghijklmnopqrstuvwxyz"
+const upperbet = alphbet.toUpperCase()
 
+const caesar = function(rawText, direction) {
+    let encrypted = ""
+    for (let i = 0; i < rawText.length; i++) {
+        let newText = rawText[i]
+        if (alphbet.includes(rawText[i])) {
+            newIndex = alphbet.indexOf(rawText[i]) + direction;
+            if (newIndex > 25 || newIndex < -25) {
+                newIndex = newIndex % 26
+            }
+            newText = alphbet.at(newIndex)
+        }
+        else if (upperbet.includes(rawText[i])) {
+            newIndex = upperbet.indexOf(rawText[i]) + direction;
+            if (newIndex > 25 || newIndex < -25) {
+                newIndex = newIndex % 26
+            }
+            newText = upperbet.at(newIndex)
+        }
+        encrypted = encrypted + newText
+    }
+    return encrypted;
 };
 
 // Do not edit below this line
